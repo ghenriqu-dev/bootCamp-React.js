c168718506e0ac8a18791942aaf177b576a7f448
ghenriqu-dev <ghenriqu.dev@gmail.com>
2023-07-10 22:19:06 -0300

    fixed lower case

--- exercicios/desafios-pre-work-b-academy/dia3/src/form.js
+++ exercicios/desafios-pre-work-b-academy/dia3/src/form.js
@@ -6,7 +6,7 @@ const capitalize = (str) => {
       if (lowerCaseWords.includes(word.toLowerCase())) {
         return word.toLowerCase()
       } else {
-        return word.charAt(0).toUpperCase() + word.slice(1)
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
       }
     }).join(' ')
 }
