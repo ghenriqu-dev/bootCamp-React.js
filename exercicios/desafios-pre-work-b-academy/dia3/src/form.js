605c568488e31bc950cf1d1d0a0df995e347b2df
ghenriqu-dev <ghenriqu.dev@gmail.com>
2023-07-11 00:06:22 -0300

    same chenges in createColor

--- exercicios/desafios-pre-work-b-academy/dia3/src/form.js
+++ exercicios/desafios-pre-work-b-academy/dia3/src/form.js
@@ -1,4 +1,6 @@
 const nameInput = document.querySelector('[data-js="name"]')
+const colorsSelection = document.querySelector('[data-js="colors"')
+const printColors = document.querySelector('[data-js="print-colors"')
 
 const capitalize = (str) => {
     return str.map(word => {
@@ -11,8 +13,27 @@ const capitalize = (str) => {
     }).join(' ')
 }
 
-nameInput.addEventListener('input', (e) =>{
+const createColor = (colors) => {
+    colors.forEach(el => {
+        const div = document.createElement('div')
+        printColors.appendChild(div)    
+    })
+}
+
+nameInput.addEventListener('input', (e) => {
     const value = capitalize(e.target.value.split(' '))
     e.target.value = value
     console.log(value)
 })
+
+colorsSelection.addEventListener('change', (e) => {
+    const colors = [...e.target.options].map(el => {
+        return {
+            color: el.value,
+            selected: el.selected
+        }
+    })
+
+    createColor(colors.filter(color => color.selected == true))
+})
+
